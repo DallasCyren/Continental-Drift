1fc34d661fbdd04b0bb1ef8fd80ef319e7e345c9
Dennis Mitchell <dennislmitchell@gmail.com>
2025-05-22 08:13:42 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,6 +1,6 @@
 /*   STEP 1   */
-export const leagueID = "1182060252741193728"; // your league ID
-export const leagueName = "Volatile Dynasty"; // your league name
+export const leagueID = "1198707757587374080"; // your league ID
+export const leagueName = "Continental Drift"; // your league name
 export const dues = 100; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
 export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
